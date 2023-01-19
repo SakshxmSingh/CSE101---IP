b19922cce625f76c21a43330399f6356bfe28802
Saksham Singh <saksham5904@gmail.com>
2023-01-19 18:41:55 +0530

    update q8 ip ass 2

--- A2/A2_Q8_2022434.py
+++ A2/A2_Q8_2022434.py
@@ -4,27 +4,34 @@
 # IP Assignment 02
 
 #--------------ques08------------------
-file = open("CSE101 - IP/A2/URLs.txt","r")
+file = open("/Users/saksham/Desktop/Programming/CSE101 - IP/A2/URLs.txt","r")
 
 basic_dict = {}
 lstlst = []
 for i in file:
-    lst = [j for j in i.split(", ")]
+    lst = [j for j in i.split(" ")]
+    lst[0] = lst[0][:5]
+    lst[1] = lst[1][:len(lst[1])-1]
+    lst[-1] = lst[-1][:5]
     lst.pop()
     set1 = set(lst[2:])
     lst2 = [lst[0],float(lst[1]),set1]
     # basic_dict.update({lst[0]:(float(lst[1]),set1)})
     lstlst.append(lst2)
-# print(basic_dict)
+
+# print(lstlst)
 
 for i in range(len(lstlst)):
-    sum = 0
+    sum = lstlst[i][1]
     for j in range(len(lstlst)):
         if lstlst[i][0] in lstlst[j][2]:
             sum+=lstlst[j][1]/len(lstlst[j][2])
     basic_dict.update({lstlst[i][0]:sum})
 
+# print(basic_dict)
+
 sort_values = [basic_dict[key] for key in basic_dict]
+# print(sort_values)
 sort_values.sort(reverse=True)
 
 n = int(input("Enter how many URLs to display: "))
