aeaf7c8ce64b45128317858b2e7bd36671f8583b
Saksham Singh <saksham5904@gmail.com>
2023-01-21 23:08:17 +0530

    Update ques.py

--- MTH100 A1/ques.py
+++ MTH100 A1/ques.py
@@ -1,4 +1,27 @@
-file = open("/Users/saksham/Desktop/Programming/CSE101 - IP/MTH100 A1/matrix.txt", "r+")
-matrix = [list(map(float,i.split())) for i in file]
-# print(matrix)
+# Saksham Singh
+# 2022434
+# Section B,  Group 7
+# MTH Assignment 01
 
+file = open("/Users/saksham/Desktop/Programming/CSE101 - IP/MTH100 A1/matrix.txt", "r+")
+mtx = [list(map(float,i.split())) for i in file]
+print(mtx)
+
+def pivot_divide(lst):
+    i = 0
+    if lst[0]==0:
+        if set(lst)=={0}:
+            pass
+        else:
+            while lst[i]==0:
+                if lst[i+1]==0:
+                    pass
+                else:
+                    lst=list(map(lambda a: a/lst[i+1],lst))
+                i+=1                
+    else:
+        lst=list(map(lambda a: a/lst[0],lst))
+    return lst
+
+
+print(pivot_divide(mtx[0]))
