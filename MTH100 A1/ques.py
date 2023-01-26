b889a8dff60bc0cf534522b71b12c6e74967c0f3
Saksham Singh <saksham5904@gmail.com>
2023-01-26 18:20:17 +0530

    mth ques update

--- MTH100 A1/ques.py
+++ MTH100 A1/ques.py
@@ -33,36 +33,36 @@ print(mtx)
 #         if mtx[i][j]
 
 
-def rref(matrix):
-    def swap(r1,r2):
-        r1, r2 = r2, r1
-        return r1, r2
+# def rref(matrix):
+#     def swap(r1,r2):
+#         r1, r2 = r2, r1
+#         return r1, r2
 
-    lead = 0
-    ro = len(matrix)
-    col = len(matrix[0])
+#     lead = 0
+#     ro = len(matrix)
+#     col = len(matrix[0])
     
-    for r in range(ro):
-        if col<=lead:
-            break
-        i=r
-        while matrix[i][lead] == 0:
-            i+=1
-            if ro == i:
-                i=r
-                lead+=1
-                if col == lead:
-                    break
-        if i!=r:
-            matrix[i],matrix[r]=swap(matrix[i],matrix[r])
-        for k in range(len(matrix[r])):
-            matrix[r][k]=matrix[r][k]/matrix[r][lead]
-        for j in range(ro):
-            if j!=r:
-                for k in range(len(matrix[j])):
-                    matrix[j][k] = matrix[r][k]*matrix[j][lead]
-        lead+=1
-    return matrix
+#     for r in range(ro):
+#         if col<=lead:
+#             break
+#         i=r
+#         while matrix[i][lead] == 0:
+#             i+=1
+#             if ro == i:
+#                 i=r
+#                 lead+=1
+#                 if col == lead:
+#                     break
+#         if i!=r:
+#             matrix[i],matrix[r]=swap(matrix[i],matrix[r])
+#         for k in range(len(matrix[r])):
+#             matrix[r][k]=matrix[r][k]/matrix[r][lead]
+#         for j in range(ro):
+#             if j!=r:
+#                 for k in range(len(matrix[j])):
+#                     matrix[j][k] = matrix[r][k]*matrix[j][lead]
+#         lead+=1
+#     return matrix
+# print(rref(mtx))
 
 
-print(rref(mtx))
